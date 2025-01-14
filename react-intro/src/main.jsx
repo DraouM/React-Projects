b469eb82351d754324c8257f8e7d6d1c5e7a3a72
mohamedaminedraou <mohamedaminedraou@gmail.com>
2025-01-14 21:38:03 +0000

    create the app component and made the neccessery updates

--- react-intro/src/main.jsx
+++ react-intro/src/main.jsx
@@ -1,55 +1,5 @@
 import { createRoot } from "react-dom/client";
+import App  from "./app";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<ReactFacts />);
-
-function Header() {
-  return (
-    <header className="header">
-      <img src="/src/assets/react.svg" alt="react logo" />
-      <nav>
-        <ul className="nav-list">
-          <li className="nav-item">Home</li>
-          <li className="nav-item">About</li>
-          <li className="nav-item">Pricing</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <main>
-      <h1>Fun facts about React</h1>
-      <ol className="facts-list">
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>
-          Is maintained by Meta and a community of individual developers and
-          companies
-        </li>
-        <li>React is a JavaScript library for building user interfaces.</li>
-        <li>React is declarative, efficient, and flexible.</li>
-        <li>
-          React can be used to build single-page applications, mobile
-          applications, and more.
-        </li>
-      </ol>
-    </main>
-  );
-}
-
-function Footer() {
-  return <footer className="footer">Copyright 2024, React Facts</footer>;
-}
-
-function ReactFacts() {
-  return (
-    <div className="card">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
+root.render(<App />);
