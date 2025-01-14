8c0dceec43c3ffcca03a2cb4bd8338e0262c3777
mohamedaminedraou <mohamedaminedraou@gmail.com>
2025-01-14 21:35:56 +0000

    add a bunch of files and assets

---
+++ react-intro/src/app.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="card container">
+      
+      {/* Add the <img> tag */}
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
