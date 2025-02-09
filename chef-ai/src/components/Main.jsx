491080b35526534221c4eb3cd92279cba24ac635
Draou Mohammed Elamine <mohamedaminedraou@gmail.com>
2025-02-09 19:50:12 +0100

    add the main section

---
+++ chef-ai/src/components/Main.jsx
@@ -0,0 +1,24 @@
+export default function Main(){
+    return (
+        <main>
+            <form action="" method="post">
+             <div className="input-div">
+                <input type="text" id="search" className="search" placeholder="Search for recepie ..." />
+                <button type="submit">Search</button>
+             </div>
+            </form>
+
+            <h2>Ingredients on hand</h2>
+
+            <ul>
+                <li>Ingrediant one </li>
+                <li>lorem lipsum lorem </li>
+                <li>some other ingrediants</li>
+                <li>lorem lipsum lorem </li>
+
+            </ul>
+
+            
+        </main>
+    )
+}
