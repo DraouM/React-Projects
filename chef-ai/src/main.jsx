256cb1a838c4d860c3ffc6b068654f218a4524aa
Draou Mohammed Elamine <mohamedaminedraou@gmail.com>
2025-02-09 19:50:26 +0100

    add some styles

--- chef-ai/src/main.jsx
+++ chef-ai/src/main.jsx
@@ -2,11 +2,14 @@ import {createRoot} from 'react-dom/client'
 import './index.css' 
 import './main.css' 
 import Header from './components/Header'
+import Main from './components/Main'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <>
+  <div className="card">
     <Header />
-    <h1>Hello world!</h1>
+    <Main />
+  </div>
   </>
 )
